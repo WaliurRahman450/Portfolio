96131bb0b7edeb5508e0bc23a26a670bdbd6525b
WaliurRahman450 <waliurrahaman450@gmail.com>
2024-05-10 22:36:45 +0600

    update

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -8,7 +8,7 @@ import emailjs from "@emailjs/browser";
 const Contact = () => {
   const form = useRef();
   const sendEmail = (e) => {
-    console.log('hello')
+    console.log("hello");
     e.preventDefault();
 
     emailjs
@@ -18,8 +18,8 @@ const Contact = () => {
       .then(
         () => {
           console.log("SUCCESS!");
-          e.target.reset()
-          alert('Your email has been sent successfully.')
+          e.target.reset();
+          alert("Your email has been sent successfully.");
         },
         (error) => {
           console.log("FAILED...", error.text);
@@ -29,15 +29,27 @@ const Contact = () => {
 
   return (
     <section className="contactPage">
-      <div id="clients">I don't have any clients yet.</div>
+      <div id="academics">
+        <h1>Educational Background</h1>
+      </div>
       <div id="contact">
         <h1 className="contactPageTitle">Contact Me</h1>
         <span className="contactDesc">
           Please fill up the form below to discuss any opportunities
         </span>
         <form className="contactForm" ref={form} onSubmit={sendEmail}>
-          <input type="text" className="name" placeholder="Your Name" name="your_name" />
-          <input type="email" className="email" placeholder="Your Email" name="your_email" />
+          <input
+            type="text"
+            className="name"
+            placeholder="Your Name"
+            name="your_name"
+          />
+          <input
+            type="email"
+            className="email"
+            placeholder="Your Email"
+            name="your_email"
+          />
           <textarea
             name="message"
             rows="5"
@@ -48,10 +60,18 @@ const Contact = () => {
             Submit
           </button>
           <div className="links">
-            <a href="https://www.facebook.com/waliur.rahman.14203544/"><img src={fb} alt="" className="link" /></a>
-            <a href="https://github.com/WaliurRahman450"><img src={git} alt="" className="link" /></a>
-            <a href="https://www.linkedin.com/in/waliur-rahman-311312247/"><img src={linkedin} alt="" className="link" /></a>
-            <a href="https://wa.me/1857326760"><img src={whatsapp} alt="" className="link" /></a>
+            <a href="https://www.facebook.com/waliur.rahman.14203544/">
+              <img src={fb} alt="" className="link" />
+            </a>
+            <a href="https://github.com/WaliurRahman450">
+              <img src={git} alt="" className="link" />
+            </a>
+            <a href="https://www.linkedin.com/in/waliur-rahman-311312247/">
+              <img src={linkedin} alt="" className="link" />
+            </a>
+            <a href="https://wa.me/1857326760">
+              <img src={whatsapp} alt="" className="link" />
+            </a>
           </div>
         </form>
       </div>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,20 +3,19 @@ import './Navigation.css'
 import menu from './menu.gif'
 import { Link } from'react-scroll';
 import { LuMessageSquare } from "react-icons/lu";
+
 const Navigation = () => {
    /* alert('This website is not fully responsive.Please use desktop for get better experience.') */
 
    const [showMenu, setShowMenu] =useState(false);
     return (
-
-        <div>
            <nav className="navbar">
                 <img src="/icon.svg" alt="" className='logo' />
                 <div className="desktopMenu">
                     <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className = "desktopMenuListItem">Home</Link>
                     <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className = "desktopMenuListItem">About</Link>
                     <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className = "desktopMenuListItem">Portfolio</Link>
-                    <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className = "desktopMenuListItem">Clients</Link>
+                    <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className = "desktopMenuListItem">Academics</Link>
                 </div>
                 <button  className="desktopMenuBtn" onClick={()=>{
                     document.getElementById('contact').scrollIntoView({behavior:'smooth'});
@@ -29,11 +28,11 @@ const Navigation = () => {
                     <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className=  "listItem"onClick = {()=>setShowMenu(false)} >Home</Link>
                     <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className=  "listItem"onClick = {()=>setShowMenu(false)} >About</Link>
                     <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className=   "listItem"onClick = {()=>setShowMenu(false)} >Portfolio</Link>
-                    <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className= "listItem"onClick = {()=>setShowMenu(false)} >Clients</Link>
+                    <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className= "listItem"onClick = {()=>setShowMenu(false)} >Academics</Link>
                     <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className= "listItem"onClick = {()=>setShowMenu(false)} >Contact</Link>
                 </div>
            </nav>
-        </div>
+    
     );
 };
 
